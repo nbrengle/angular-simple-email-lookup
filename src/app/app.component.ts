78f80ddba7c9b8521a0579b34b4c9e31b6dd579e
nathaniel <nathaniel@playstudios.com>
2017-09-26 14:17:06 -0700

    initial commit with essential functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:
+  `
+    <div class="field">
+        <input name="query" placeholder="Enter First and Last Name" #searchElement>
+    </div>
+    <button (click)="search(searchElement)">
+        Search
+    </button>
+    <p> {{ searchResultEmail }} </p>
+    <ul>
+        <li *ngFor="let key of keys()">
+            {{ key }}
+        </li>
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  @Input() searchElement: string;
+  classList;
+  searchResultEmail;
+  errorMessage: string;
+
+  constructor(private _http: Http) {}
+
+  keys() : Array<string> {
+      return Object.keys(this.classList);
+  }
+
+  search(searchElement: HTMLInputElement) {
+      if (this.keys().includes(searchElement.value)) {
+          console.log(this.classList[searchElement.value]);
+          this.searchResultEmail = this.classList[searchElement.value];
+
+      } else
+      {
+          console.log("no match");
+          this.searchResultEmail = "NO MATCH NAME. PLEASE TRY AGAIN";
+      }
+  }
+
+  ngOnInit() {
+      this._http.get('/assets/class-email-dictionary.json')
+          .subscribe(
+              res => {
+                  this.classList = <IEmailListing>res.json()
+                  console.log(res.json())
+              },
+              error => this.errorMessage = <any>error
+          );
+  }
+}
+
+export class IEmailListing {
+    [ index: string ] : string;
 }
